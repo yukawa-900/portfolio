aa59548be938fb4a5e96303fe49d09bc88f58c27
Yukawa <67470479+yukawa-900@users.noreply.github.com>
2020-11-04 19:01:02 +0900

    initial commit
    DockerfileとDocker-compose.ymlを追加。Djangoのsettings.pyを編集。

---
+++ backend/config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls))] + urlpatterns

354b93012c142b00bfd84e17d552492e96d26632
Yukawa <67470479+yukawa-900@users.noreply.github.com>
2020-11-14 22:14:33 +0900

    :sparkles: コンフリクト調整用

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,16 +26,12 @@ env.read_env(env_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'jazzmin',

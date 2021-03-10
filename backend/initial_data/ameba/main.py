9d7d95deca578acc688212d907500ef4af3dfa68
Yukawa <67470479+yukawa-900@users.noreply.github.com>
2021-03-10 18:09:48 +0900

    :construction: サンプルデータの作成
    モデル間のon_deleteを見直す必要性に気づいたため、データの作成を中断した

---
+++ backend/initial_data/ameba/main.py
@@ -0,0 +1,19 @@
+from ameba.models import AmebaDepartment, \
+                    SalesUnit, \
+                    CostItem, \
+                    Employee, \
+                    SalesByItem, \
+                    SalesByCategory, \
+                    Cost, \
+                    WorkingHours, \
+                    SalesCategory
+from django.contrib.auth import get_user_model
+
+# def main():
+#     user = get_user_model()(
+#         email="sample@gmail.com"
+#     )
+
+    # dept1 = AmebaDepartment(
+    #     name
+    # )
